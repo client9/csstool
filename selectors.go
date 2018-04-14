385613926ebc15425d1fc4fe08200aabd28b396b
nickg <nickg@signalsciences.com>
2018-04-13 23:52:31 -0700

    rip out key function and test

---
+++ selectors.go
@@ -0,0 +1,74 @@
+package csstool
+
+import (
+	"github.com/tdewolff/parse/css"
+)
+
+func primarySelector(tokens []css.Token) []byte {
+	buf := []byte{}
+	hasClass := false
+	for _, t := range tokens {
+		switch t.TokenType {
+		case css.IdentToken, css.HashToken:
+			buf = append(buf, t.Data...)
+			continue
+		case css.ColonToken:
+			// three cases,
+			// foo:hover
+			// ::after
+			// :root
+			if len(buf) == 0 || (len(buf) == 1 && buf[0] == ':') {
+				buf = append(buf, t.Data...)
+				continue
+			}
+			return buf
+		case css.LeftBracketToken:
+			// we only handle case of raw
+			// [ foo ]
+			// not
+			// a[foo]
+			if len(buf) == 0 {
+				buf = append(buf, t.Data...)
+				continue
+			}
+			return buf
+		case css.RightBracketToken:
+			buf = append(buf, t.Data...)
+			return buf
+		case css.WhitespaceToken:
+			// really a delimiter
+			return buf
+		case css.DelimToken:
+			if len(t.Data) != 1 {
+				panic("got delim with len > 1")
+			}
+			if t.Data[0] == '.' {
+				// only allow one class:
+
+				if hasClass {
+					return []byte{}
+				}
+				hasClass = true
+				buf = append(buf, t.Data...)
+				continue
+			}
+			if t.Data[0] == '*' {
+				buf = append(buf, t.Data...)
+				continue
+			}
+			return buf
+		default:
+			buf = append(buf, t.Data...)
+		}
+	}
+	return buf
+}
+
+// gives the complete identier from tokens
+func completeSelector(tokens []css.Token) []byte {
+	buf := []byte{}
+	for _, t := range tokens {
+		buf = append(buf, t.Data...)
+	}
+	return buf
+}
---
+++ selectors_test.go
@@ -0,0 +1,75 @@
+package csstool
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/tdewolff/parse/css"
+)
+
+func TestGetSelectors(t *testing.T) {
+
+	cases := []struct {
+		src  string
+		want string
+	}{
+		{
+			"p",
+			"p",
+		},
+		{
+			"a:hover",
+			"a",
+		},
+		{
+			"p.foo",
+			"p.foo",
+		},
+		{
+			".foo",
+			".foo",
+		},
+		{
+			// multiple class selector not supported
+			"p.foo.bar",
+			"",
+		},
+		{
+			".foo.bar",
+			"",
+		},
+		{
+			"[ hidden ]",
+			"[hidden]",
+		},
+		{ // special case
+			"*",
+			"*",
+		},
+		{
+			":root",
+			":root",
+		},
+		{
+			"::after",
+			"::after",
+		},
+		{
+			"#foo",
+			"#foo",
+		},
+		{
+			"p#foo",
+			"p#foo",
+		},
+	}
+
+	for i, c := range cases {
+		p := css.NewParser(strings.NewReader(c.src+"{}"), false)
+		p.Next()
+		got := string(primarySelector(p.Values()))
+		if got != c.want {
+			t.Errorf("case %d: want %q got %q", i, c.want, got)
+		}
+	}
+}
