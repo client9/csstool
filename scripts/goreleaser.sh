6b441bc81a8e71111aa078f928166be1d6193c1b
nickg <nickg@signalsciences.com>
2018-04-20 22:23:36 -0700

    fix goreleaser build

--- scripts/goreleaser.sh
+++ scripts/goreleaser.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -e
 # autorelease based on tag
-test -n "$TRAVIS_TAG" && curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | bash
+if [ -n "$TRAVIS_TAG" ]; then
+	curl -sfL https://install.goreleaser.com/github.com/goreleaser/goreleaser.sh | bash
+	./bin/goreleaser
+fi
