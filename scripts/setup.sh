e50d871a2b0529e0ddaff4b0feeaababddd1cf54
nickg <nickg@signalsciences.com>
2018-04-22 06:19:20 -0700

    build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-curl -sSL https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-curl -sSL https://install.goreleaser.com/github.com/alecthomas/gometalinter.sh | sh
-curl -sSL https://install.goreleaser.com/github.com/client9/misspell | sh
+curl -sfSL https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
+curl -sfSL https://install.goreleaser.com/github.com/alecthomas/gometalinter.sh | sh
+curl -sfSL https://install.goreleaser.com/github.com/client9/misspell.sh | sh
