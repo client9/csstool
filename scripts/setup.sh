9ddd427af3be66babc579cd9da5100a614473fbf
nickg <nickg@signalsciences.com>
2018-10-25 01:56:30 -0700

    fix gometalinter?

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 set -ex
 curl -sfSL https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-curl -sfSL https://install.goreleaser.com/github.com/alecthomas/gometalinter.sh | sh
+curl -L https://git.io/vp6lP | sh
 curl -sfSL https://install.goreleaser.com/github.com/client9/misspell.sh | sh
-go get golang.org/x/tools/cmd/goimports
-
