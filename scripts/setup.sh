e584e18812c89fa561f228942597b21fe97f6c32
nickg <nickg@client9.com>
2018-11-30 21:21:12 -0800

    make setup more robust

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
 curl -sfSL https://raw.githubusercontent.com/golang/dep/master/install.sh | sh
-curl -L https://git.io/vp6lP | sh
+curl -sfSL https://git.io/vp6lP | sh
 curl -sfSL https://install.goreleaser.com/github.com/client9/misspell.sh | sh
